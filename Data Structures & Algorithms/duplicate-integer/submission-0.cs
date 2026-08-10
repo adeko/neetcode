a3a22c31edf4cfdad5d30b1ecb3d52b3afe3951c
adeko <al.degtyarenko@gmail.com>
2026-08-10 17:27:57 +0300

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        var hs = new HashSet<int>();
+        foreach (var n in nums) if (!hs.Add(n)) return true;
+        return false;
+    }
+}
